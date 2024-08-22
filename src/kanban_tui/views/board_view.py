c356f6a1d65a9cdd792097334e649d5fcf819c93
Zaloog <gramslars@gmail.com>
2024-08-22 21:13:40 +0200

    create extra file for modal

--- src/kanban_tui/views/board_view.py
+++ src/kanban_tui/views/board_view.py
@@ -7,49 +7,47 @@ from textual.containers import Horizontal
 
 from kanban_tui.widgets.task_column import Column
 from kanban_tui.widgets.task_card import TaskCard
-from kanban_tui.views.task_view import TaskEditScreen
+from kanban_tui.modal.modal_task_screen import TaskEditScreen
 
 from kanban_tui.constants import COLUMNS
 
 
 class KanbanBoard(Horizontal):
     BINDINGS = [
-        ("space", "place_task", "new task")
+        ("n", "new_task", "New"),
+        ("e", "edit_task", "Edit"),
         # ("shift+j", "reposition_task",)
     ]
 
     row_focused: reactive[int | None] = reactive(None)
     column_focused: reactive[int | None] = reactive(None)
     position: reactive[tuple[int]] = reactive((0, 0))
+    # selected_task: Task
 
     def compose(self) -> Iterable[Widget]:
         for column in ["Ready", "Doing", "Done"]:
             yield Column(title=column)
         return super().compose()
 
-    def action_place_task(self, task: TaskCard | None = None) -> None:
+    def action_new_task(self) -> None:
         self.app.push_screen(TaskEditScreen())
-        # self.query_one("#column_ready", Column).task_amount += 1
-        # ta = self.query_one("#column_ready", Column).task_amount
-        # card = TaskCard(title=f"Task {ta}", row=ta - 1, column=0)
-        # self.query_one("#column_ready", Column).query_one(VerticalScroll).mount(card)
+
+    def action_edit_task(self, task: TaskCard | None = None) -> None:
+        self.app.push_screen(TaskEditScreen(task=task))
 
     def key_j(self):
         column = self.query(Column)[self.position[1]]
         row = (self.position[0] + 1) % column.task_amount
-
         column.query(TaskCard)[row].focus()
 
     def key_k(self):
         column = self.query(Column)[self.position[1]]
         row = (self.position[0] + column.task_amount - 1) % column.task_amount
-
         column.query(TaskCard)[row].focus()
 
     def key_l(self):
         row = self.position[0]
         column = self.query(Column)[(self.position[1] + 1) % len(COLUMNS)]
-
         try:
             column.query(TaskCard)[row].focus()
         except IndexError:
@@ -58,7 +56,6 @@ class KanbanBoard(Horizontal):
     def key_h(self):
         row = self.position[0]
         column = self.query(Column)[(self.position[1] + 2) % len(COLUMNS)]
-
         try:
             column.query(TaskCard)[row].focus()
         except IndexError:
@@ -69,4 +66,5 @@ class KanbanBoard(Horizontal):
         self.position = event.taskcard.position
 
     def watch_position(self):
+        # self.selected_task =
         self.log.error(self.position)
