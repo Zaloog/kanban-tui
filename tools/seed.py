104ffe212f43592351c2c16b84398c92ad3b4bb5
Zaloog <gramslars@gmail.com>
2024-09-07 09:11:15 +0200

    improve seed script

--- tools/seed.py
+++ tools/seed.py
@@ -1,19 +1,48 @@
 from kanban_tui.database import create_new_task_db, init_new_db
+from kanban_tui.config import KanbanTuiConfig, init_new_config
+
+from kanban_tui.constants import DB_FULL_PATH, CONFIG_FULL_PATH
 
 
 def main():
+    CONFIG_FULL_PATH.unlink(missing_ok=True)
+    init_new_config()
+
+    cfg = KanbanTuiConfig()
+
+    cfg.add_category(
+        category="blue",
+        color="#0000FF",
+    )
+    cfg.add_category(
+        category="red",
+        color="#FF0000",
+    )
+    cfg.add_category(
+        category="green",
+        color="#00FF00",
+    )
+
+    DB_FULL_PATH.unlink(missing_ok=True)
     init_new_db()
+    # Ready
     create_new_task_db(
-        title="Test_Task_Ready", description="Hallo", category="Work", column=0
+        title="Task_green_ready", description="Hallo", category="green", column=0
     )
     create_new_task_db(
-        title="Test_Task_Ready", description="Hallo", category="Work", column=0
+        title="Task_blue_ready", description="Hallo", category="blue", column=0
     )
     create_new_task_db(
-        title="Test_Task_Doing", description="Hallo", category="Work", column=1
+        title="Task_none_ready", description="Hallo", category=None, column=0
     )
+
+    # Doing
     create_new_task_db(
-        title="Test_Task_Done", description="Hallo", category="Work", column=2
+        title="Task_green_doing", description="Hallo", category="green", column=1
+    )
+    # Done
+    create_new_task_db(
+        title="Task_red_done", description="Hallo", category="red", column=2
     )
 
 
