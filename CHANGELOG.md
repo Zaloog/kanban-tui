f2a0f088bb27fa075f0d1c6ec24e6a9067f7b585
Zaloog <gramslars@gmail.com>
2024-09-29 20:32:19 +0200

    change dev setup database path to xdg

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,7 @@
 # Changelog
 
+## v0.1.1
+- switch database and config path from `CWD` for developing to `XDG`
+
 ## v0.1.0
 - published on PyPi
--- src/kanban_tui/constants.py
+++ src/kanban_tui/constants.py
@@ -1,18 +1,18 @@
 from pathlib import Path
-# from platformdirs import user_config_dir, user_data_dir
+from platformdirs import user_config_dir, user_data_dir
 
 COLUMNS = ["Ready", "Doing", "Done", "Archive"]
 
 DB_NAME = "database.db"
-DB_LOCATION = Path().cwd()
-# DB_LOCATION = Path(
-#     user_data_dir(appname="kanban-tui", appauthor=False, ensure_exists=True)
-# )
+# DB_LOCATION = Path().cwd()
+DB_LOCATION = Path(
+    user_data_dir(appname="kanban-tui", appauthor=False, ensure_exists=True)
+)
 DB_FULL_PATH = DB_LOCATION / DB_NAME
 
 CONFIG_NAME = "kanban_tui.yaml"
-CONFIG_LOCATION = Path().cwd()
-# CONFIG_LOCATION = Path(
-#     user_config_dir(appname="kanban-tui", appauthor=False, ensure_exists=True)
-# )
+# CONFIG_LOCATION = Path().cwd()
+CONFIG_LOCATION = Path(
+    user_config_dir(appname="kanban-tui", appauthor=False, ensure_exists=True)
+)
 CONFIG_FULL_PATH = CONFIG_LOCATION / CONFIG_NAME
