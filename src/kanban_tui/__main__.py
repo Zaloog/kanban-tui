ed699dcf22987d8c80708e3f75dd5f66c22ce1bb
Zaloog <gramslars@gmail.com>
2024-10-12 14:55:29 +0200

    add click and cli options

--- src/kanban_tui/__main__.py
+++ src/kanban_tui/__main__.py
@@ -1,10 +1,48 @@
+import click
 from kanban_tui.app import KanbanTui
 
+from kanban_tui.constants import (
+    TEMP_CONFIG_FULL_PATH,
+    TEMP_DB_FULL_PATH,
+    DB_FULL_PATH,
+    CONFIG_FULL_PATH,
+)
 
-def main():
-    app = KanbanTui()
+
+@click.group(
+    context_settings={"ignore_unknown_options": True}, invoke_without_command=True
+)
+@click.version_option(prog_name="kanban-tui")
+@click.pass_context
+def cli(ctx):
+    if ctx.invoked_subcommand is None:
+        app = KanbanTui()
+        app.run()
+    else:
+        pass
+
+
+@cli.command("demo")
+def run_demo_app():
+    """
+    Start a Demo App with temporary DB and Config
+    """
+    app = KanbanTui(config_path=TEMP_CONFIG_FULL_PATH, database_path=TEMP_DB_FULL_PATH)
     app.run()
+
+    TEMP_CONFIG_FULL_PATH.unlink(missing_ok=True)
+    TEMP_DB_FULL_PATH.unlink(missing_ok=True)
+
+
+@cli.command("clear")
+def delete_database_and_config():
+    """
+    Deletes DB and Config
+    """
+
+    CONFIG_FULL_PATH.unlink(missing_ok=True)
+    DB_FULL_PATH.unlink(missing_ok=True)
 
 
 if __name__ == "__main__":
-    main()
+    cli()
--- src/kanban_tui/constants.py
+++ src/kanban_tui/constants.py
@@ -1,18 +1,30 @@
-from pathlib import Path
-from platformdirs import user_config_dir, user_data_dir
+from platformdirs import user_config_path, user_data_path, user_runtime_path
 
 COLUMNS = ["Ready", "Doing", "Done", "Archive"]
 
 DB_NAME = "database.db"
-# DB_LOCATION = Path().cwd()
-DB_LOCATION = Path(
-    user_data_dir(appname="kanban-tui", appauthor=False, ensure_exists=True)
-)
-DB_FULL_PATH = DB_LOCATION / DB_NAME
-
 CONFIG_NAME = "kanban_tui.yaml"
+
+# For Developing
+# DB_LOCATION = Path().cwd()
 # CONFIG_LOCATION = Path().cwd()
-CONFIG_LOCATION = Path(
-    user_config_dir(appname="kanban-tui", appauthor=False, ensure_exists=True)
+
+# Normal Use
+DB_LOCATION = user_data_path(appname="kanban-tui", appauthor=False, ensure_exists=True)
+CONFIG_LOCATION = user_config_path(
+    appname="kanban-tui", appauthor=False, ensure_exists=True
 )
+
+DB_FULL_PATH = DB_LOCATION / DB_NAME
 CONFIG_FULL_PATH = CONFIG_LOCATION / CONFIG_NAME
+
+# Demo
+TEMP_DB_LOCATION = user_runtime_path(
+    appname="kanban-tui", appauthor=False, ensure_exists=True
+)
+TEMP_DB_FULL_PATH = TEMP_DB_LOCATION / DB_NAME
+
+TEMP_CONFIG_LOCATION = user_runtime_path(
+    appname="kanban-tui", appauthor=False, ensure_exists=True
+)
+TEMP_CONFIG_FULL_PATH = TEMP_CONFIG_LOCATION / CONFIG_NAME
