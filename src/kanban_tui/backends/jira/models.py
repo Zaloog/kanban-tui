41769600e3626caf5366ad4f2addfeff82f9c08f
Zaloog <gramslars@gmail.com>
2025-11-12 04:48:25 +0100

    Add comment to jira models

--- src/kanban_tui/backends/jira/models.py
+++ src/kanban_tui/backends/jira/models.py
@@ -1,10 +1,11 @@
 from pydantic import BaseModel
+# TODO
+# Use a new model or reuse existing one?
 
 
 class JiraIssue(BaseModel):
     assignee: str
     key: str
-    ...
 
 
 class JiraProject(BaseModel): ...
